d7cd1c58f6249832713c3ee796a796e7a3dd7f18
icrxz <igor.cruz@mercadolivre.com>
2021-09-24 21:40:47 -0300

    add missing routes

--- src/router/routes/project.routes.ts
+++ src/router/routes/project.routes.ts
@@ -14,4 +14,12 @@ projectRouter.post("/updateData", (req, resp) => projectController.updateData(re
 
 projectRouter.post("/insertData", (req, resp) => projectController.insertData(req, resp))
 
+projectRouter.get("/checkLogin", (req, resp) => projectController.checkLogin(req, resp))
+
+projectRouter.get("/confirmSpot", (req, resp) => projectController.confirmSpot(req, resp))
+
+projectRouter.post("/cancelSpot", (req, resp) => projectController.cancelSpot(req, resp))
+
+projectRouter.post("/checkStatusSpot", (req, resp) => projectController.checkStatusSpot(req, resp))
+
 export default projectRouter;
