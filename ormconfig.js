29f9b2d39831fb22a8fbcfa2f279aeec41688d22
icrxz <igor_cruz_avelino@hotmail.com>
2021-10-24 00:46:48 -0300

    change database configs

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,21 @@
 module.exports = {
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
-  "entities": [
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  logging: false,
+  extra: {
+    ssl: true,
+  },
+  entities: [
     "src/models/**/*.ts"
- ],
- "migrations": [
-  "src/config/database/migrations/**/*.ts"
-],
- "cli":{
-  "migrationsDir": [
-    "src/config/database/migrations/"
   ],
-  "entitiesDir": "src/models"
+  migrations: [
+    "src/config/database/migrations/**/*.ts"
+  ],
+  cli:{
+    migrationsDir: [
+      "src/config/database/migrations/"
+    ],
+    entitiesDir: "src/models"
   }
 }
